6035aa635cff253cf83e5f36dfbecb5a92a7aff8
unknown <luo_x@.ad.psb.bls.gov>
2013-07-17 13:39:52 -0400

    update p22

--- p22.rb
+++ p22.rb
@@ -1,15 +1,31 @@
-def proper_div(n)
-  sum = 0
-  1.upto(n-1) do |i|
-    sum += i if n%i == 0   
+$name = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+arr = Array.new
+
+File.open("names.txt", "r").each_line do |line|
+  line.split(",").each {|i| arr << i.gsub('"', "")}
+end
+
+def name_value(name)
+  sum_n = 0
+  name.split("").each do |n|
+    sum_n += $name.index(n) + 1
   end
-  return sum
+  return sum_n
 end
 
-sum = 0 
-1.upto(9999) do |x|
-  y = proper_div(x)
-    sum += x if proper_div(y) == x && x!=y
+$h = Hash.new
+def name_cache(arr)
+  arr.sort.each do |n|
+    $h[n] = name_value(n) unless $h[n]
+  end
 end
 
-puts sum
+name_cache(arr)
+
+sum = 0
+arr.sort.each_with_index do |n, i|
+  sum += $h[n] * (i+1)
+end
+
+p sum
