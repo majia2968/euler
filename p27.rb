18f0d3096cbaa716da944a1229ddc0ac78c1207e
chris <majia3080@gmail.com>
2013-07-24 17:54:24 -0400

    update p27

--- p27.rb
+++ p27.rb
@@ -1,18 +1,30 @@
 require 'prime'
 arr = []
-(-999).upto(999) do |a|
-  (-999).upto(999) do |b|
-    n = 0
-    while true
-      if (n**2 + a*n + b).prime?
-        n += 1
-      else
-        break
+arr1 = {}
+tmp = []
+for a in (-999..999) 
+  if a.abs.prime?
+      for b in 0..999 do
+        if b.prime? 
+          n = 0
+          while true
+            m = n**2 + a*n + b
+            if m.prime?
+              n += 1
+            else
+              break
+            end
+          end
+          arr << n 
+          arr1[n] = a*b
+        end
       end
-    end
-   arr << n 
   end
 end
 
+
+
 p arr.max
+p arr1[arr.max]
+
         
