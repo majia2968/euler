58f628d5efca99621b1636bb36281de3878ddebf
unknown <luo_x@.ad.psb.bls.gov>
2013-08-12 11:23:28 -0400

    update p37

--- p37.rb
+++ p37.rb
@@ -1,24 +1,3 @@
-require 'prime'
+require "prime"
 
-(11..1000).each do |i|
-  next unless i.prime?
-  
-end
-
-def truncatable(i)
-  tmp = i.to_s
-  while tmp.length > 0
-    arr = tmp.split("")
-    arr.shift
-    p tmp
-    if arr.join.to_i.prime?
-      tmp = arr.join
-    elsif arr.join.to_i.prime? and tmp.length == 1
-      return true
-      break
-    else
-      return false
-    end
-  end
-end
-p truncatable(3797)
+p (10..1000000).select {|i| (0..(i.to_s.length-1)).all? {|j| i.to_s[0..j].to_i.prime? && i.to_s[j..-1].to_i.prime? }}.reduce(:+)
